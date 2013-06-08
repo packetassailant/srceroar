0672154b851fc76597b34d724801debb7d3d6487
packetassailant <cpatten@packetresearch.com>
2013-06-08 11:58:02 -0400

    Initial commit to check functionality between GIT and IDE env

--- srceroar/srceroar.js
+++ srceroar/srceroar.js
@@ -1,4 +1,17 @@
-/**
- * New node file
+/*
+ CODE: SRCEROAR
+ AUTHOR: Chris Patten
+ LANG: Node.js/JavaScript
+ EMAIL: cpatten[t.a.]packetresearch.com
+ TWITTER: packetassailant
+ 
+ FUNCTION: Explode source code archives into a word-list comprised of unique words.
+ 		   This is useful for targeted attacks against open-source implementations.
+ 		   Examples of use might be when deriving admin functionality through forceful browsing. 
  */
 fs = require('fs');
+
+var data = fs.readFileSync(filepath, 'utf-8');
+
+
+
