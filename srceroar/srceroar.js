922dbc69bb9d6c55668a08babe549c9522a1ebe1
packetassailant <cpatten@packetresearch.com>
2013-07-29 10:30:14 -0400

    Cleaned up with working callbacks/objects

--- srceroar/srceroar.js
+++ srceroar/srceroar.js
@@ -1,14 +1,15 @@
 /*
- CODE: 		SRCEROAR
- AUTHOR: 	Chris Patten
- LANG: 		Node.js/JavaScript
- EMAIL: 	cpatten[t.a.]packetresearch.com
- TWITTER: 	packetassailant
+CODE:		SRCEROAR
+AUTHOR:		Chris Patten
+LANG:		NodeJS/JavaScript
+EMAIL:		cpatten[t.a.]packetresearch.com
+TWITTER:	packetassailant
  
- FUNCTION: 	Explode source code archives into a word-list comprised of unique words.
- 			This is useful for targeted attacks against open-source implementations.
- EXAMPLES:  Locating privileged functionality in non-standard installs through forceful browsing. 
+FUNCTION:	Explode source code archives into a word-list comprised of unique words.
+			This is useful for targeted attacks against open-source implementations.
+EXAMPLE:	Locating privileged functionality in non-standard installs through forceful browsing. 
 */
+
 var fs = require('fs');
 var tarball = require('tarball-extract');
 var http = require('http');
@@ -16,66 +17,72 @@ var https = require('https');
 var url = require('url');
 var optimist = require('optimist')
 	.usage('Create wordlists from source code archives (e.g., src-tarball.tar.gz).\
-    		\nUsage: $0 (-i archive || -u download-url) -o wordlist-output-file')
-    		.alias('h', 'help')
-    		.alias('i', 'infile')
-    		.alias('o', 'outfile')
-    		.alias('u', 'url')    			
-    		.describe('h', 'Show Usage and opts (i.e., this list of opt/args)')
-    		.describe('i', 'Input archive file')
-    		.describe('o', 'Output wordlist file')
-    		.describe('u', 'Input archive download URL (e.g., http://nodejs.org/dist/v0.10.12/node-v0.10.12.tar.gz)');
+    	\nUsage: $0 (-i archive || -u download-url) -o wordlist-output-file')
+    	.alias('h', 'help')
+    	.alias('i', 'infile')
+    	.alias('o', 'outfile')
+    	.alias('u', 'url')    			
+    	.describe('h', 'Show Usage and opts (i.e., this list of opt/args)')
+    	.describe('i', 'Input archive file')
+    	.describe('o', 'Output wordlist file')
+    	.describe('u', 'Input archive download URL (e.g., http://nodejs.org/dist/v0.10.12/node-v0.10.12.tar.gz)');
 
 
 function PathObj() { }
 PathObj.prototype = {
   	setinputfile: function (inputfile) {
   		this.inputfile = inputfile;
-		extractArchive(this.inputfile, recurseDirectory);
+  		console.log(this.inputfile);
+		//extractArchive(this.inputfile);
+		//return this.inputfile;
 	},
 	setoutputfile: function (outputfile) {
 		this.outputfile = outputfile;
-		createOutFile(this.outputfile);
+		console.log(this.outputfile);
+		//createOutFile(this.outputfile);
+		//return this.outputfile;
 	},
-	setdownloadurl: function (flag, downloadurl) {
-		this.flag = flag;
+	setdownloadurl: function (downloadurl) {
 		this.downloadurl = downloadurl;
-		downloadArchive(this.flag, this.downloadurl, extractArchive);
+		console.log(this.downloadurl);
+		//downloadArchive(this.flag, this.downloadurl, extractArchive);
+		//return this.downloadurl;
+	},
+	settmpdirectory: function () {
+		this.tmppath = '/tmp/' + Math.random().toString(36).substr(2,9);
+		//return this.tmppath;
 	}
 };
 
 function validatePath(flag, pathentry, callback) {
 	var pdtmp = pathentry;
-	var resultUrl = pathentry.match(/^http/);
-	fs.exists(pathentry, function(pathentry) {
-		if (flag === "urlflag" && resultUrl) {
-			if (typeof callback === "function") {
-				return callback(flag, resultUrl.input);
-			}
-		}
-		if (flag === "inflag" && pathentry) {
-			fs.stat(pdtmp, function(err, stats) {
-				if (err) {
-					throw err; 
-				}
-				if (stats.isFile) {
-					if (typeof callback === "function") {
-						return callback(pdtmp);
-					}					
-				} 
-			});
-		}
-		if (flag === "outflag") {
-			fs.exists(pdtmp, function(pdtmp) {
-				if (pdtmp) {
-					if (typeof callback === "function") {
-						return callback(pdtmp);
-					}
-				}
-			});
+	var urlregex = /^http/;
+	if (flag === "urlflag" && urlregex.test(pdtmp)) {
+		if (typeof callback === "function") {
 			return callback(pdtmp);
 		}
-	});
+	}
+	if (flag === "inflag" && pathentry) {
+		fs.stat(pdtmp, function(err, stats) {
+			if (err) {
+				throw err;
+			}
+			if (stats.isFile) {
+				if (typeof callback === "function") {
+					return callback(pdtmp);
+				}
+			}
+		});
+	}
+	if (flag === "outflag") {
+		fs.exists(pathentry, function(pathentry) {
+			if (pathentry === true) {
+				if (typeof callback === "function") {
+					return callback(pdtmp);
+				}
+			}
+		});
+	}
 }
 
 function createOutFile(outputFile) {
@@ -146,23 +153,19 @@ function downloadArchive(flag, urlFile, callback){
 	}	 
 }
 
-function extractArchive(archiveFile, callback) {
-	if (archiveFile.match(/\.tar/)) {
-		var fileuuid = Math.random().toString(36).substr(2,9);
-	} else if (archiveFile.match(/\.tgz/)) {
-		var fileuuid = Math.random().toString(36).substr(2,9);
+function extractArchive(archiveFile) {
+	if (archiveFile.match(/\.tar|tgz/)) {
+		tarball.extractTarball(archiveFile, '/tmp/' + fileuuid, function(err){
+			if (err) {
+				throw err;
+			} else {
+				recurseDirectory("test");
+			}
+		})
 	} else {
 		console.log("The file %s contains an unsupported extension!", archiveFile);
 		process.exit(0);
-	}
-
-	tarball.extractTarball(archiveFile, '/tmp/' + fileuuid, function(err){
-		if (err) {
-			throw err;
-		} else if (typeof callback === "function") {
-			return callback("test");
-		} 
-	})
+	}	
 }
 
 function recurseDirectory(inFile) {
@@ -174,21 +177,23 @@ function main() {
 	var pathinst = new PathObj();
 
 	if (argv.help) {
-		showhelp();
+		optimist.showHelp();
 	}
 
 	if (argv.infile && argv.url) {
 	    console.info('Error! --infile and --url are mutually exclusive\n');
-	    showhelp();
+	    optimist.showHelp();
 	}
 
 	if (argv.infile) {
 		var inflag = "inflag";
+		pathinst.settmpdirectory();
 		validatePath(inflag, argv.infile, pathinst.setinputfile);
 	}
 
 	if (argv.url) {
 		var urlflag = "urlflag";
+		pathinst.settmpdirectory();
 		validatePath(urlflag, argv.url, pathinst.setdownloadurl);
 	}
 
